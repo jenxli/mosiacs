7fb0067fab11fa981036893a8eeac88168f437c0
hengolivia <53484921+oliviaaheng@users.noreply.github.com>
2025-04-23 11:58:43 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -12,12 +12,12 @@ while 1:
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
+    mosaic = img # place holder
     # Stacks the video feeds for the comparison
-    comparison_video = np.hstack((img, img))
+    comparison_video = np.hstack((img, mosaic))
     cv2.imshow('screen', comparison_video)
 
     # Use esc to exit
