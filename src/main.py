45652b3dad87115cb8267eedbad6550228573b30
hengolivia <53484921+oliviaaheng@users.noreply.github.com>
2025-04-23 11:54:48 -0400

    added comparison video feed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import cv2 
+import numpy as np
 
+# Set up camera and face cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 capture = cv2.VideoCapture(0)
 
@@ -14,8 +16,9 @@ while 1:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
-
-    cv2.imshow('img',img)
+    # Stacks the video feeds for the comparison
+    comparison_video = np.hstack((img, img))
+    cv2.imshow('screen', comparison_video)
 
     # Use esc to exit
     k = cv2.waitKey(30) & 0xff 
